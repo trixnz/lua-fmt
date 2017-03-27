000228b6033f16793e4a53062edf95e36da81f0a
trixnz <me@trix.nz>
2017-03-27 21:36:20 +0200

    Add tests for functions

---
+++ test/function/functions.lua
@@ -0,0 +1,14 @@
+function test() end
+function test(arg) end
+function test(arg, ...) end
+function test(...) end
+
+local function test() end
+local function test(arg) end
+local function test(arg, ...) end
+local function test(...) end
+
+local test = function() end
+local test = function(arg) end
+local test = function(arg, ...) end
+local test = function(...) end
