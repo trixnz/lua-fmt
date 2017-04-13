7a673db8ee246450839e9319e9aa9c9eae150fb8
trixnz <me@trix.nz>
2017-04-13 23:09:48 +0200

    Add patching to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,9 @@ var gulp = require('gulp');
 var tslint = require('gulp-tslint');
 var shell = require('gulp-shell');
 
-// var bump = require('gulp-bump')
-// var git = require('gulp-git');
-// var tag_version = require('gulp-tag-version');
+var bump = require('gulp-bump')
+var git = require('gulp-git');
+var tag_version = require('gulp-tag-version');
 
 var files = {
     src: [
@@ -13,13 +13,13 @@ var files = {
     ],
 };
 
-// function bumpVersion(ver) {
-//     return gulp.src(['./package.json'])
-//         .pipe(bump({ type: ver }))
-//         .pipe(gulp.dest('./'))
-//         .pipe(git.commit('Bump package version'))
-//         .pipe(tag_version());
-// }
+function bumpVersion(ver) {
+    return gulp.src(['./package.json'])
+        .pipe(bump({ type: ver }))
+        .pipe(gulp.dest('./'))
+        .pipe(git.commit('Bump package version'))
+        .pipe(tag_version());
+}
 
 gulp.task('compile', shell.task([
     'npm run compile'
@@ -37,8 +37,8 @@ gulp.task('test', shell.task([
     'npm test'
 ]));
 
-// gulp.task('patch', ['default'], function () { return bumpVersion('patch'); })
-// gulp.task('minor', ['default'], function () { return bumpVersion('minor'); })
-// gulp.task('major', ['default'], function () { return bumpVersion('major'); })
+gulp.task('patch', ['default'], function () { return bumpVersion('patch'); })
+gulp.task('minor', ['default'], function () { return bumpVersion('minor'); })
+gulp.task('major', ['default'], function () { return bumpVersion('major'); })
 
 gulp.task('default', ['compile', 'tslint']);
