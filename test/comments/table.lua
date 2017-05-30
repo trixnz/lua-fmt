c94d462350e86e4048da7233de76062c4b2b799c
Will Moffat <will@hamstersoup.com>
2017-05-30 19:48:48 +0000

    Failing test: trailing comments are missing indent level.
    A comment on the last line of a table or function seems to miss a
    level of indentation.

---
+++ test/comments/table.lua
@@ -0,0 +1,17 @@
+-- Simple example
+a = {
+    b = true
+    -- Trailing comment should be indented
+}
+
+-- Deep example
+film = {
+    scene = {
+        object = {
+            name = "Swallow",
+            carrying = "Coconut",
+            origin = "Africa"
+            -- velocity = ??,
+        }
+    }
+}
