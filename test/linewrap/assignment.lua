8119c3a2b7eae8eb150ead584db4d61e4bdef7da
Will Moffat <will@hamstersoup.com>
2017-06-07 07:19:50 +0200

    Failing test: line length not respected in assignment.

--- test/linewrap/assignment.lua
+++ test/linewrap/assignment.lua
@@ -1,2 +1,8 @@
 bbbbbbbbbbbbbbbbbbbbbbb = true
 aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa = bbbbbbbbbbbbbbbbbbbbbbb
+
+local function foo()
+   return 1, 2, 3
+end
+local a, bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb, c = foo()
+print(a, bbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbbb, c)
