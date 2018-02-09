2e39dca3de805bc354c7dd4d90a911f297c9f304
Otto Robba <otavio.robba@tapps.com.br>
2018-02-09 11:43:58 -0200

    Format long line on testPrinter

--- src/testPrinter.ts
+++ src/testPrinter.ts
@@ -4,5 +4,9 @@ import * as fs from 'fs';
 
 const file = fs.readFileSync('test/lua-5.3.4-tests/calls.lua');
 
-const formatted = luafmt.formatText(file.toString(), { lineWidth: 60, quotemark: 'single' });
+const formatted = luafmt.formatText(file.toString(), {
+    lineWidth: 60,
+    quotemark: 'single'
+});
+
 console.log(formatted);
